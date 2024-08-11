5fed9beaf2be514facbb79d105d616b228cab670
mrinmoyborah <mrinmoyborah26100@gmail.com>
2024-08-11 18:56:32 +0530

    GET request of spending created

--- Controller/spendController.js
+++ Controller/spendController.js
@@ -7,13 +7,24 @@ async function handlePostUserSpending(req, res){
             res.status(400).json({"errorMessage": "Provide all data"})
         }
         const spend = await Spend.create({amount: req.body.amount, paymentType: req.body.paymentType, date: Date.now()})
-        res.status(200).json({"successMessage": "Inserted successfully"})
+        res.status(200).json({"successMessage": "Inserted successfully", "data": spend})
     }catch(error){
         res.status(500).json({"errorMessage": error})
     }
 }
- 
-async function handleGetUserSpending(req, res){}
+
+async function handleGetUserSpending(req, res){
+    try{
+        const allTransactions = await Spend.find()
+        if(!allTransactions){
+            return res.status(400).json({"errorMessage": "Not available"})
+        }else{
+            return res.status(200).json({"allTransaction": allTransactions})
+        }
+    }catch(error){
+        res.status(500).json({"errorMessage": error})
+    }
+}
 
 module.exports = {
     handleGetUserSpending,
